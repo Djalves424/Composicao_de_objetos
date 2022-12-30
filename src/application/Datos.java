87b939f2255c3f5d679a078fb8b158e532504c65
Diego Alves <djalves424@gmail.com>
2022-12-30 03:27:47 -0300

    Composição de objetos

---
+++ src/application/Datos.java
@@ -0,0 +1,21 @@
+package application;
+
+public class Datos {
+	/* "DADOS A SEREM DIGITADOS POR EXEMPLO"
+	 * 
+	 *  Enter client data:
+		Name: nome do cliente
+		Email: email do cliente
+		Birth date (DD/MM/YYYY): data de nascimento do cliente
+		Enter order data:
+		Status: PROCESSING
+		How many items to this order? 2
+		Enter #1 item data:
+		Product name: TV
+		Product price: 1000.00
+		Quantity: 1
+		Enter #2 item data:
+		Product name: Mouse
+		Product price: 40.00
+		Quantity: 2*/
+}
